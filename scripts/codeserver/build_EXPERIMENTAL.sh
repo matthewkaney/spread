f93991db568f812df980d8b85bd8df06bcc63c1a
Caleb <calebegg@google.com>
2020-02-21 13:22:56 -0500

    Updates to experimental codeserver script

--- scripts/codeserver/build_EXPERIMENTAL.sh
+++ scripts/codeserver/build_EXPERIMENTAL.sh
@@ -13,11 +13,21 @@ nvm --version
 nvm install 10
 nvm use 10
 
+rm -rf ~/TEMP/code-server
 git clone https://github.com/cdr/code-server.git ~/TEMP/code-server
 cd ~/TEMP/code-server
+
+LATEST=`git describe --tags --abbrev=0`
+VSC_VERSION=`echo $LATEST | sed -n 's/.*vsc\(.*\)/\1/p'`
+
+# Switch to latest tagged version (assuming only releases are tagged)
+git checkout tags/$LATEST
+
 yarn install
 
-yarn build 1.39.2 2
-yarn binary 1.39.2 2
+echo $VSC_VERSION
+
+yarn build $VSC_VERSION 2
+yarn binary $VSC_VERSION 2
 
 nvm use node
