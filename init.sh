64639da41cf2f9000ff5181541927f23e8d53c63
Matthew <matthew@mindofmatthew.com>
2020-03-18 14:33:15 -0400

    Fix init

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
     sudo rm -rf /usr/local/codeserver
 
     # Install new version
-    git clone https://github.com/mindofmatthew/spread.git /usr/local/spread
+    sudo git clone https://github.com/mindofmatthew/spread.git /usr/local/spread
 
     # Set up symlink to spread script
     sudo ln -s /usr/local/spread/util/spread.sh /usr/local/bin/spread
